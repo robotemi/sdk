2396e642beba942dcbde55454c73a5c6776f24e3
Charlie Zhang <charlie.zhang@robotemi.com>
2024-08-13 12:06:46 +0800

    Fix Greek STT language code

--- sdk/src/main/java/com/robotemi/sdk/SttLanguage.kt
+++ sdk/src/main/java/com/robotemi/sdk/SttLanguage.kt
@@ -26,7 +26,7 @@ enum class SttLanguage(val value: Int) {
     MS_MY(22), // Supported from 134 version
     VI_VN(23), // Supported from 134 version
     RU_RU(24), // Supported from 134 version
-    EL_GR(24), // Supported from 134 version
+    EL_GR(25), // Supported from 134 version
     ;
 
     companion object {
